6b2610a4ff88d7f5cf352fb0cbcc92f4e774cdbb
Rajiv PR <48161947+rajivprao@users.noreply.github.com>
2026-07-03 00:32:01 +0530

    Implement verdict_node for investment assessment
    Refactor ScoringNode to create verdict_node function for generating investment assessment report. Integrate ChatAgent for LLM interaction and save verdict to JSON file.

--- app/graphs/nodes/verdict.py
+++ app/graphs/nodes/verdict.py
@@ -1,26 +1,40 @@
-from langchain_core.prompts import ChatPromptTemplate
-from schemas.score_schema import MarketScorecard
-from prompts.scoring_prompt import SCORING_SYSTEM_PROMPT
+from app.prompts.verdict_prompt import VERDICT_PROMPT
+from app.graphs.state import LaunchLensState
+from app.agents.chat_agent import ChatAgent
+import json
 
 
-class ScoringNode:
+user_template = """
+Generate the final LaunchLens investment assessment report using the following JSON.
 
-    def __init__(self, llm):
+The JSON contains:
 
-        self.chain = (
-            ChatPromptTemplate.from_messages(
-                [
-                    ("system", SCORING_SYSTEM_PROMPT),
-                    ("human", "{market_assessment}")
-                ]
-            )
-            | llm.with_structured_output(MarketScorecard)
-        )
+- Business idea
+  {idea}
+- Opportunity analysis and Evidence scorecard
+  {opportunity}
 
-    def invoke(self, opportunity_analysis):
+Determine the verdict using the scoring rules defined in the system prompt.
 
-        return self.chain.invoke(
-            {
-                "market_assessment": opportunity_analysis
-            }
-        )
+Return only valid JSON.
+"""
+
+async def verdict_node(state: LaunchLensState):
+
+    idea = state["idea_context"]
+    opportunity = state["opportunity_analysis"]
+    user_inputs = {"idea":idea,"opportunity":opportunity}
+
+    user_prompt = user_template.format(**user_inputs)
+
+    system_instruction = VERDICT_PROMPT
+    llm = ChatAgent()
+    response = llm.run(query=user_prompt,system_instruction=system_instruction)
+    verdict = json.loads(response)
+  
+    # Open file in append mode ('a')
+    with open('tests/verdict.json', 'w', encoding='utf-8') as file:
+        json.dump(verdict, file, indent=4)
+
+    return {"verdict":verdict}
+
